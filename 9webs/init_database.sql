4e2dab3b0847390d32b1de701793732432578182
zhengrr <afoolsbag@gmail.com>
2020-09-23 19:57:48 +0800

    feat(9webs): 更新至 9webs 0.5 数据库结构

--- 9webs/init_database.sql
+++ 9webs/init_database.sql
@@ -5,20 +5,24 @@
  * 初始化 9webs 数据库
  *
  * zhengrr
- * 2020-09-07 – 2020-09-15
+ * 2020-09-07 – 2020-09-19
  * Unlicense
  */
 
--- 创建 spider 用户
+-- 创建 spider 和 digger 用户
 DO $PLpgSQL$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname='spider') THEN
         CREATE USER spider
           WITH PASSWORD 'password';
     END IF;
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname='digger') THEN
+        CREATE USER digger
+          WITH PASSWORD 'password';
+    END IF;
 END
 $PLpgSQL$;
 
--- 创建 spider_database 数据库
-CREATE DATABASE spider_database
+-- 创建 9webs_database 数据库
+CREATE DATABASE "9webs_database"
   WITH ENCODING 'UTF8';
--- 9webs/init_tables.sql
+++ 9webs/init_tables.sql
@@ -5,11 +5,13 @@
  * 初始化 9webs 数据表
  *
  * zhengrr
- * 2020-09-07 – 2020-09-17
+ * 2020-09-07 – 2020-09-19
  * Unlicense
  */
 
+--
 -- 创建 aliexpress_product_information 表
+--
 CREATE TABLE IF NOT EXISTS aliexpress_product_information (
     product_id     TEXT NOT NULL PRIMARY KEY,
     category_id    TEXT,
@@ -20,51 +22,83 @@ CREATE TABLE IF NOT EXISTS aliexpress_product_information (
     price_current  TEXT,
     price_big_sale TEXT
 );
-GRANT ALL
+
+GRANT INSERT, SELECT, UPDATE
    ON TABLE aliexpress_product_information
    TO spider;
 
+GRANT SELECT
+   ON TABLE aliexpress_product_information
+   TO digger;
+
+--
 -- 创建 aliexpress_search_results 表
+--
+
 CREATE TABLE IF NOT EXISTS aliexpress_search_results (
     search_text  TEXT      NOT NULL,
     search_time  TIMESTAMP NOT NULL,
     result_index INTEGER   NOT NULL,
     product_id   TEXT      NOT NULL,
-    sale_value   TEXT,
+    sale_value   BIGINT,
     PRIMARY KEY (search_text, search_time, result_index)
 );
+
 CREATE INDEX aliexpress_search_results_product_id_index
     ON aliexpress_search_results (product_id);
-GRANT ALL
+
+GRANT INSERT
    ON TABLE aliexpress_search_results
    TO spider;
 
+GRANT SELECT
+   ON TABLE aliexpress_search_results
+   TO digger;
+
+--
 -- 创建 amazon_product_information 表
+--
+
 CREATE TABLE IF NOT EXISTS amazon_product_information (
-    asin           TEXT NOT NULL PRIMARY KEY,
-    uuid           TEXT,
-    image_url      TEXT,
-    title          TEXT,
-    ourprice       TEXT,
-    price          TEXT
+    asin       TEXT NOT NULL PRIMARY KEY,
+    uuid       TEXT,
+    image_url  TEXT,
+    title      TEXT,
+    ourprice   TEXT,
+    price      TEXT,
+    department TEXT
 );
-GRANT ALL
+
+GRANT INSERT, SELECT, UPDATE
    ON TABLE amazon_product_information
    TO spider;
 
+GRANT SELECT
+   ON TABLE amazon_product_information
+   TO digger;
+
+--
 -- 创建 amazon_search_results 表
+--
+
 CREATE TABLE IF NOT EXISTS amazon_search_results (
     search_text      TEXT      NOT NULL,
     search_time      TIMESTAMP NOT NULL,
     result_index     INTEGER   NOT NULL,
     asin             TEXT      NOT NULL,
-    stars            TEXT,
-    count_of_ratings TEXT,
-    sales_rank       TEXT,
+    decistars        SMALLINT,           -- “分星”，即 1/10 星
+    count_of_ratings BIGINT,
+    sales_rank       BIGINT,
     PRIMARY KEY (search_text, search_time, result_index)
 );
+
 CREATE INDEX amazon_search_results_asin_index
     ON amazon_search_results (asin);
-GRANT ALL
+
+GRANT INSERT
    ON TABLE amazon_search_results
    TO spider;
+
+GRANT SELECT
+   ON TABLE amazon_search_results
+   TO digger;
